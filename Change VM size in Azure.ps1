3481ae4fffd76ad635542d81f3dd91e1fa69c3f8
Harold Zubieta Bengolea <57451670+HaroldZubietaB@users.noreply.github.com>
2020-12-13 15:50:09 -0500

    Add files via upload

---
+++ Change VM size in Azure.ps1
@@ -0,0 +1,9 @@
+﻿#Get data of current VMs in suscription
+Get-AzVm
+
+#Configure variables that will help us change the size
+$vm = Get-AzVM -ResourceGroupName RGName -VMName SQLLinux
+$vm.HardwareProfile.VmSize = "Standard_B2ms"
+
+#Change the size of VM
+Update-AzVM -VM $vm -ResourceGroupName ResourceGroup1
